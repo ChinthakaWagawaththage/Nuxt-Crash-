b833df2c3fc95d72a1d51c6204eda95422ac048c
Chinthaka_W <chinthakaprasad789@gmail.com>
2021-08-31 00:02:27 +0530

    Added Components Nav, SmallCard and SmallCardDisplay

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="container">
+    <Nav />
     <Hero></Hero>
     <LargeCardDisplay
     v-for="cardInfo in largeCardInfo"
     :key="cardInfo.id"
     :cardSection="cardInfo"
     />
+    <SmallCardDisplay
+      v-for="cardInfo in smallCardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+      />
   </div>
 </template>
 
 <script>
-import { largeCardSections } from '@/assets/data.js'
+import { largeCardSections, smallCardSections } from '@/assets/data.js'
 export default {
   data () {
     return {
-      largeCardInfo: largeCardSections
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
     }
   }
 }
